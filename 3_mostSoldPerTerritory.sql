df95d690b1f5fa73a808b842971321c96c40fb1a
Jorge Salgado <jrsalgado@asesoftware.com>
2023-02-24 14:19:40 -0500

    Point 3 finished (hopefuly ok this time)

---
+++ 3_mostSoldPerTerritory.sql
@@ -0,0 +1,42 @@
+/*
+Listar el top 10 de productos mas vendidos por territorio en el ultimo mes.
+*/
+
+/*
+Necesito crear el count a partir de SalesOrderHeader
+
+SalesOrderHeader:
+    * TerritoryID (territorio de la compra)
+    * SalesOrderID (FK a detalles de la compra)
+    * OrderDate
+SalesOrderDetailID:
+    * ProductID: id del producto
+*/
+
+DECLARE @lastMonthEnd DATE;
+SET @lastMonthEnd = '2014-06-30';
+DECLARE @lastMonthBegin DATE;
+SET @lastMonthBegin = '2014-06-01';
+
+
+WITH
+
+    ProductsCountInTerritoryInLastMonth
+    AS
+
+    (
+        SELECT
+            orderHeader.TerritoryID,
+            orderDetails.ProductID AS ProductID,
+            COUNT(ProductID) SalesCount,
+            ROW_NUMBER() OVER(PARTITION BY TerritoryID ORDER BY COUNT(ProductID) DESC) ranking
+        FROM Sales.SalesOrderHeader orderHeader
+            INNER JOIN Sales.SalesOrderDetail orderDetails ON orderDetails.SalesOrderID = orderHeader.SalesOrderID
+        WHERE 
+            OrderDate<= @lastMonthEnd AND OrderDate>= @lastMonthBegin
+        GROUP BY TerritoryID, ProductID 
+
+    )
+SELECT *
+FROM ProductsCountInTerritoryInLastMonth
+WHERE ranking<11;
